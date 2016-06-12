db4ee08190abe992b8f3d33a9107e569fdf884ae
Tom Kidd <tomjkidd@gmail.com>
2016-06-12 11:30:17 -0400

    Add Exercises 5.5 - 5.7

--- Exercises.hs
+++ Exercises.hs
@@ -22,3 +22,34 @@ f1 fs n = map (\f -> f n) fs
 
 exercise5_4 :: [Int]
 exercise5_4 = f1 (f2 (*) [1,2,3,4]) 5
+
+{- Exercise 5.5 -}
+{- NOTE: Skipping recursive definitions -}
+doubleEach :: [Int] -> [Int]
+doubleEach xs = map (\x -> 2 * x) xs
+
+pairAndOne :: [Int] -> [(Int,Int)]
+pairAndOne xs = map (\x -> (x, x+1)) xs
+
+addEachPair :: [(Int,Int)] -> [Int]
+addEachPair xs = map (\(x,y) -> x + y) xs
+
+exercise5_5 :: ([Int], [(Int, Int)], [Int])
+exercise5_5 = (doubleEach [1,2,3], pairAndOne [1,2,3], addEachPair [(1,2), (3,4), (5,6)])
+
+{- Exercise 5.6 -}
+
+maxList :: [Int] -> Int
+maxList [] = error "maxList can not be use on []"
+maxList xs = foldr (\x acc -> max x acc) (head xs) xs
+
+minList :: [Int] -> Int
+minList [] = error "minList can not be used on []"
+minList xs = foldr (\x acc -> min x acc) (head xs) xs
+
+{- Exercise 5.7 -}
+addPairsPointwise :: [(Int, Int)] -> (Int, Int)
+addPairsPointwise xs = foldr (\(x,y) (accx, accy) -> (x + accx, y + accy)) (0,0) xs
+
+exercise5_7 :: (Int, Int)
+exercise5_7 = addPairsPointwise [(1,2), (3,4), (5,6)]
